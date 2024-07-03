18dc33d21006ed91021addb212c171b593fd5100
Alex Sokolov <Gelezako@users.noreply.github.com>
2024-07-03 20:52:35 +0300

    update BaseDesktopPage

--- lib/desktop/pages/base_desktop_page.ts
+++ lib/desktop/pages/base_desktop_page.ts
@@ -1,16 +1,17 @@
 import { Page } from '@playwright/test'
 import { BasePage } from '@lib/desktop/pages/base_page'
-// import { Footer } from '../../components/footer/footer'
-// import { Header } from '../../components/header/header'
+import { Header } from '@lib/desktop/components/header'
+import { Footer } from '@lib/desktop/components/footer'
+
 
 export class BaseDesktopPage extends BasePage {
-	// header: Header
-	// footer: Footer
+	header: Header
+	footer: Footer
 
 	constructor(page: Page, pageName: string, pageUrl?: string) {
 		super(page, pageName, pageUrl)
 
-		// this.header = new Header(this.page.locator('.css-pux5gx'))
-		// this.footer = new Footer(this.page.locator('.css-zv7ju9'))
+		this.header = new Header(this.page.locator('.css-pux5gx'))
+		this.footer = new Footer(this.page.locator('.css-zv7ju9'))
 	}
 }
